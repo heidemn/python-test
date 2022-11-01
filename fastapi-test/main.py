0a8cba7301714ebee39f121ac537854698852efd
Martin Heide <martin.heide@gmx.net>
2022-11-01 21:58:25 +0100

    Black

--- fastapi-test/main.py
+++ fastapi-test/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 @app.head("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
